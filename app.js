9e5001092c071194ad28702adca99027be52d79d
ChakradharReddy-Putta <puttachakradharreddy.com>
2020-09-18 21:57:18 +0530

    Created user model and connected to the database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,8 +10,25 @@ const routes = require("./routes/route");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(4000);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://Chakradhar:Chinnu@143@cluster0.c2li5.mongodb.net/News?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((result) => {
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
